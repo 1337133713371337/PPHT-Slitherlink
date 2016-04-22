d0351e42879c92e09970ad746d0b2861e73a256a
Trọng Đinh <Trng inh>
2016-04-22 17:56:19 +0700

    solve law 2

--- Slitherlink/src/SatEncode.java
+++ Slitherlink/src/SatEncode.java
@@ -16,12 +16,46 @@ public class SatEncode {
 		val = v;
 		k = w * (h + 1);
 	}
+	int up (int r, int c) {
+		return w * r + c + 1;
+	}
+	int down (int r, int c) {
+		return w * (r + 1) + c + 1;
+	}
+	int right (int r, int c) {
+		return k + (w + 1) * r + c + 2;
+	}
+	int left (int r, int c) {
+		return k+ (w + 1) * r + c + 1;
+	}
+	int edgeUp (int r, int c) {
+		if (r == 0) return 0;
+		if (c != 0) return right(r-1, c-1);
+		return left(r-1, c);
+	}
+	int edgeRight(int r, int c) {
+		if (c == w) return  0;
+		if (r != h) return up(r, c);
+		return down(r-1, c);
+	}
+	int edgeDown (int r, int c) {
+		if (r == h) return 0;
+		if (c != w) return left(r, c);
+		return right(r, c-1);
+	}
+	int edgeLeft (int r, int c) {
+		if (c == 0) return  0;
+		if (r != 0) return down(r-1, c-1);
+		return up(r, c-1);
+	}
 	public ArrayList<String> getEncodeText () {
 		return encodeText;
 	}
 	public void encode () {
 		encodeText = new ArrayList<String>();
-		//encode here
+
+		//encode law 1
+
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
 				switch (val[i][j]) {
@@ -30,14 +64,14 @@ public class SatEncode {
 						encodeText.add("-" + down(i,j));
 						encodeText.add("-" + left(i,j));
 						encodeText.add("-" + right(i,j));
-                        break;
+						break;
 					case 1 :
 						encodeText.add(up(i,j) + " " + down(i,j)+ " " + left(i,j)+ " " + right(i, j));
 						encodeText.add("-" + right(i,j) + " -" + up(i,j));
 						encodeText.add("-" + right(i,j) + " -" + left(i,j));
 						encodeText.add("-" + up(i,j) + " -" + left(i,j));
 						encodeText.add("-" + up(i,j) + " -" + down(i,j));
-						encodeText.add("-" + left(i,j) + " -" + up(i,j));
+						encodeText.add("-" + left(i,j) + " -" + down(i,j));
 						encodeText.add("-" + down(i,j) + " -" + right(i,j));
 						break;
 					case 2 :
@@ -62,18 +96,37 @@ public class SatEncode {
 				}
 			}
 		}
-	}
-	int up (int r, int c) {
-		return w * r + c + 1;
-	}
-	int down (int r, int c) {
-		return w * (r + 1) + c + 1;
-	}
-	int right (int r, int c) {
-		return k + (w + 1) * r + c + 2;
-	}
-	int left (int r, int c) {
-		return k+ (w + 1) * r + c + 1;
+		//encode law 2
+		for (int i = 0; i < w + 1; i++) {
+			for (int j = 0; j < h + 1; j++) {
+				if (edgeRight(i,j) != 0 && edgeUp(i,j) != 0 && edgeLeft(i,j) != 0 && edgeDown(i,j) != 0) {
+					encodeText.add("-" + edgeRight(i,j) + " "+edgeUp(i,j) +" " + edgeLeft(i,j) + " " + edgeDown(i,j));
+					encodeText.add("-" + edgeUp(i,j)  + " " + edgeRight(i,j) + " " + edgeLeft(i,j) + " " + edgeDown(i,j));
+					encodeText.add("-" + edgeLeft(i,j) + " "+edgeRight(i,j) +" " + edgeUp(i,j) + " " + edgeDown(i,j));
+					encodeText.add("-" + edgeDown(i,j) + " " + edgeRight(i,j) + " " + edgeUp(i,j) + " " + edgeLeft(i,j));
+					encodeText.add("-" + edgeRight(i,j) + " -" + edgeUp(i,j) + " -" + edgeLeft(i,j) + " -" + edgeDown(i,j));
+
+				}
+				if (edgeRight(i,j) != 0 && edgeUp(i,j) != 0 && edgeLeft(i,j) != 0) {
+					encodeText.add(" -" + edgeRight(i,j) + " -" + edgeUp(i,j) + " -" + edgeLeft(i,j));
+
+				}
+				if (edgeRight(i,j) != 0 && edgeUp(i,j) != 0 && edgeDown(i,j) != 0) {
+					encodeText.add(" -" + edgeRight(i,j) + " -" + edgeUp(i,j) + " -" + edgeDown(i,j));
+
+				}
+
+				if (edgeRight(i,j) != 0 && edgeLeft(i,j) != 0 && edgeDown(i,j) != 0) {
+					encodeText.add("-" + edgeRight(i,j) + " -" + edgeLeft(i,j) + " -" + edgeDown(i,j) );
+
+				}
+				if (edgeUp(i,j) != 0 && edgeLeft(i,j) != 0 && edgeDown(i,j) != 0) {
+					encodeText.add("-" + edgeUp(i,j) + " -" + edgeLeft(i,j) + " -" + edgeDown(i,j) );
+
+				}
+
+			}
+		}
 	}
 
 }
